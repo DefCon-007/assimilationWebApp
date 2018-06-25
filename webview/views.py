612eed4775c7efab4851e3e06c81923d5cf1a04a
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-06-25 16:16:32 +0530

    Adds homepage and login for webview

--- webview/views.py
+++ webview/views.py
@@ -1,12 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.decorators import api_view
 
-@api_view(["get"])
+@login_required()
 def index(request) :
     print(request.user)
-    return  render(request,'./registration/login.html')
+    if request.user.has_perm('api.add_event') :
+        print("User can change/add events")
+        createEventPermission = True
+    else :
+        createEventPermission = False
+    context = {
+        'createEventPermission' : createEventPermission
+    }
+    print(context)
+    return  render(request,'webview/index.html',context)
 
 
 @api_view(["GET"])
