47e971dcaebeae85c3b53e1571051548ec076d12
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-07-30 21:00:39 +0530

    Adds api for marking multiple attendance Improve admin ui

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,61 @@
 from django.contrib import admin
 from .models import  *
 
-modelList = [attendance,UserDeviceIdAndAuthToken, complaint]
-admin.site.register(modelList)
+# modelList = []
+# admin.site.register(modelList)
+# #
+# class EventList(admin.ModelAdmin) :
+#
+#
 
-class EventList(admin.ModelAdmin) :
-    filter_horizontal = ['helpers']
-
-admin.site.register(event,EventList)
 # Register your models here.
+
+#
+#
+# class EmailExtractTempelateResource(resources.ModelResource):
+#     class Meta:
+#         model = EmailExtractTempelate
+
+
+# class EmailExtractTempelateAdmin(ImportExportMixin, admin.ModelAdmin):
+#     resource_class = EmailExtractTempelateResource
+#     list_filter = ["query"]
+#     list_display = []
+#     for columnName in EmailExtractTempelate._meta.fields :
+#         list_display.append(columnName.get_attname_column()[0])
+#     list_display.remove("emailhtml")
+# # Register your models here.
+# class EmailExtractDataResource(resources.ModelResource):
+#     class Meta:
+#         model = EmailExtractedData
+class attendanceAdmin(admin.ModelAdmin) :
+    list_display = []
+    list_filter = ["attendanceStatus","event"]
+    for columnName in attendance._meta.fields :
+        list_display.append(columnName.get_attname_column()[0])
+
+admin.site.register(attendance,attendanceAdmin)
+
+
+class eventAdmin(admin.ModelAdmin) :
+    list_display = []
+    list_filter = ["datetime","audience","title"]
+    filter_horizontal = ['helpers']
+    for columnName in event._meta.fields :
+        list_display.append(columnName.get_attname_column()[0])
+
+admin.site.register(event,eventAdmin)
+
+
+class complaintAdmin(admin.ModelAdmin) :
+    list_display = []
+    list_filter = ["createdDateTime","event","resolutionStatus"]
+    for columnName in complaint._meta.fields :
+        list_display.append(columnName.get_attname_column()[0])
+
+
+class UserDeviceAdmin(admin.ModelAdmin) :
+    list_display = []
+    for columnName in UserDeviceIdAndAuthToken._meta.fields :
+        list_display.append(columnName.get_attname_column()[0])
+admin.site.register(UserDeviceIdAndAuthToken,UserDeviceAdmin)
